d7655776d496a338ad8e058da7db60961dca3df5
ahmadammarm <ahmad.ammar.2205356@students.um.ac.id>
2025-08-29 20:24:56 +0700

    feat: decorator lesson

---
+++ src/app.controller.ts
@@ -0,0 +1,20 @@
+/* eslint-disable prettier/prettier */
+import { Controller, Get, Post } from '@nestjs/common';
+import { AppService } from './app.service';
+
+@Controller()
+export class AppController {
+    constructor(private readonly appService: AppService) { }
+
+    // decorator: informasi tambahan (metadata) yang biasanya diawali dengan @
+    // decorator digunakan di Class, Function, Constructor, dan Parameter
+    @Get()
+    getHello(): string {
+        return this.appService.getHello();
+    }
+
+    @Post()
+    createHello(): string {
+        return this.appService.postHello();
+    }
+}
